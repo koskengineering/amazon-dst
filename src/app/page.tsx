50a499fccb450f552ab87e31e9bfa383451b087d
koskengineering <155873900+koskengineering@users.noreply.github.com>
2024-06-11 22:36:17 +0900

    SP-API連携機能作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,47 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  const [isActive, setIsActive] = useState(false);
+  const [copySuccess, setCopySuccess] = useState('');  // コピー成功メッセージ用の状態
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    if (params.has("spapi_oauth_code")) {
+      setIsActive(true);
+    }
+  }, []);
+
+  const handleButtonClick = async () => {
+    const url = new URL(window.location.href);
+    const spapi_oauth_code = url.searchParams.get("spapi_oauth_code");
+
+    // クリップボードにspapi_oauth_codeをコピー
+    if (navigator.clipboard && spapi_oauth_code) {  // navigator.clipboardが利用可能でspapi_oauth_codeが存在する場合
+      try {
+        await navigator.clipboard.writeText(spapi_oauth_code);
+        setCopySuccess("コピーしました!!");
+      } catch (err) {
+        setCopySuccess("コピーに失敗しました。");
+      }
+    } else {
+      setCopySuccess("クリップボードにアクセスできません。");
+    }
+  };
+
   return (
-    <h1>Test</h1>
+    <div>
+      <h1>"Amazon DST" SP-API Authorization Page</h1>
+      <Link href="https://sellercentral.amazon.co.jp/apps/authorize/consent?application_id=amzn1.sp.solution.4f4d0d53-3b9a-4158-88ec-c2118bafc164" legacyBehavior>
+        <a target="_blank">Amazon SP-API アクセス権限設定</a>
+      </Link>
+      <br />
+      <button onClick={handleButtonClick} disabled={!isActive}>
+        Amazon連携コードをコピー
+      </button>
+      {copySuccess && <p>{copySuccess}</p>}
+    </div>
   );
 }
